a4b9c5d94cd89db19e6b52b24ae93bee0274be10
Jatin D <b4jatin@gmail.com>
2020-07-24 19:17:17 +1000

    Welcomepage updated

--- src/About.js
+++ src/About.js
@@ -6,10 +6,21 @@ function About(){
     return(
         <div className="about-container">
             <main className="about-text">
-                <p className='address'>Hello,</p>
-                <p className='intro'>I am Jatin</p>
-                <p className='details'>A Full-Stack Web Developer based in Melbourne, Australia.</p>
-                <p className='details'>Love to draw, create and make things run.</p>
+                <div className="about-intro">
+                    <div className="about-intro-text-wrapper">
+                        <p className='address'>Hello,</p>
+                        <p className='intro'>I am Jatin</p>
+                        <p className='details'>A Full-Stack Web Developer based in Melbourne, Australia.</p>
+                        <p className='details'>Love to draw, create and make things run.</p>
+                    </div>
+                    <div className="about-intro-navigation">
+                        <div className='about-intro-navigation-link' href="">Profile and prowesses</div>
+                        <div className='about-intro-navigation-link' href="">A kaleidoscopic journey</div>
+                        <div className='about-intro-navigation-link' href="">Education</div>
+                        <div className='about-intro-navigation-link' href="">Coding endeavors</div>
+                        <div className='about-intro-navigation-link' href="">Get in touch</div>
+                    </div>
+                </div>
             </main>
             <aside className='about-animation-container'>
                 <AboutAnimate />
