6105ccea7ae4678863377a17253044ab70893d12
Jatin D <b4jatin@gmail.com>
2020-08-10 15:43:13 +1000

    Read me file added

--- README.md
+++ README.md
@@ -1,68 +1,56 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Welcome to Jatin's web portfolio!
+===================
 
-## Available Scripts
+**jatindhandhalya.com** is my portfolio website created using create react app. Portfolio website is intend to provide visitors information about my self including my personal and professional journey so far. Intention of this website is also to demonstrate my react web development capabilities. 
 
-In the project directory, you can run:
+Please visit my website by clicking
+-------------
+[Here](https://jatindhandhalya.com/)
 
-### `npm start`
+----------
 
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+Landing Page
+-------------
+As you click on the link above, you will land on welcome page of my website. Welcome page contains a welcome message and few animated introductory lines about me. While viewing it on desktop, you will see a colorful animation covering right side of the page. The animation is built using react code and I intend to update it time to time to make it better and more attractive.
 
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
+![enter image description here](https://i.imgur.com/F65ozEM.png)
 
-### `npm test`
+As you can see in the image above, landing page also includes links to major content of the website such as profile, portfolio, contact and a download link to my resume.
 
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+Profile Page
+-------------
 
-### `npm run build`
+Profile page presents general summery about myself and link to my Experience and Education sections. On the right hand side, I have created a CSS grid of icons of the skills I have. I intend to improvise this grid in some kind of animation in future. 
 
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
+![enter image description here](https://i.imgur.com/oZSJNCd.png)
 
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
+Portfolio Page
+-------------
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
 
-### `npm run eject`
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
 
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
 
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+Contact Page
+-------------
 
-## Learn More
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
 
-To learn React, check out the [React documentation](https://reactjs.org/).
 
-### Code Splitting
+----------
 
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
+Development
+-------------
+The game is developed using front-end programming languages including HTML5, CSS, and JavaScript. HTML is used to render the game on a browser. Game styles are created using CSS. All functionality is created using JavaScript.
 
-### Analyzing the Bundle Size
+> **Development Features:**
 
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
+> - To make the grid clickable, a unique class was assigned to each grid. The box containing the game interface was further divided into a 3x3 grid to create 9 boxes assigned with a unique class.
+> - Necessary styling was created to highlight not yet played boxes as well as played boxes.
+> - Created a  display message informing the winner.
+> - It has a reset button to <i class="icon-refresh"></i> **reset the game** within the current tournament and a button to <i class="icon-refresh"></i> **reset scores** after the tournament.
+> - The code tracks any clicks that happen on the cells. It checks if a valid move has been made.
+> - It makes sure that nothing happens if an already played cell has been clicked. It updates the game state and validates the game.
+> - After validation, if we have a winner, it stops the game. The code reflects the updates made on the UI and resets based on user action.
 
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
--- src/Profile.js
+++ src/Profile.js
@@ -23,102 +23,79 @@ export default function Profile(props) {
                     <div className="profile-text"> 
                         <p>I am an aspiring full-stack developer who loves to create and make things work. A mechanical engineer who believes in quality products delivered on time. A people person who is experienced in working with project teams globally.</p> 
                     </div>
-                   
-                    <h6 className="profile-skills-heading">I can work with...</h6>
-                    <div className="profile-skills-list">
-                        <div>
-                            <p className="profile-skills-list-items">React.js</p>
-                            <p className="profile-skills-list-items">Node.js</p>
-                            <p className="profile-skills-list-items">jQuery</p>
-                            <p className="profile-skills-list-items">JavaScript</p>
-                            <p className="profile-skills-list-items">HTML5/CSS3</p>
-                        </div>
-                        <div>
-                            <p className="profile-skills-list-items">Ruby</p>
-                            <p className="profile-skills-list-items">Sinatra</p>
-                            <p className="profile-skills-list-items">PostgreSQL</p>
-                            <p className="profile-skills-list-items">Underscore.js</p>
-                            <p className="profile-skills-list-items">Postman</p>
-                        </div>
-                        <div>
-                            <p className="profile-skills-list-items">Restful WS</p>
-                            <p className="profile-skills-list-items">AWS</p>
-                            <p className="profile-skills-list-items">VS Code</p>
-                            <p className="profile-skills-list-items">GitHub</p>
-                            <p className="profile-skills-list-items">Heroku</p>
-                        </div>
-                    </div>
                     <div className="profile-link-wrapper">
                         <Link className="router-link profile-links" to="/experience">Know more about experience <i className="far fa-arrow-alt-circle-right"></i></Link>
                         <Link className="router-link profile-links" to="/education">Know more about eduction <i className="far fa-arrow-alt-circle-right"></i></Link>
                     </div>
                 </div>
                 </BounceInLeft>
-                
-
-                
                 <div className="profile-skills-icons-wrapper">   
                 <BounceInRight>  
                     <div className="skill-icon-container">
                         <div className="skil-icon-wrapper">
                             <i className="devicon-react-original leng-icon"></i>
+                            <p className="profile-skills-list-items">ReactJS</p>
                         </div>
                         <div className="skil-icon-wrapper">
                             <i className="devicon-nodejs-plain leng-icon"></i>
+                            <p className="profile-skills-list-items">NodeJS</p>
                         </div>
                         <div className="skil-icon-wrapper">
                             <i className="devicon-javascript-plain leng-icon"></i>
+                            <p className="profile-skills-list-items">JavaScript</p>
                         </div>
                         <div className="skil-icon-wrapper">
                             <i className="devicon-ruby-plain leng-icon"></i>
+                            <p className="profile-skills-list-items">Ruby</p>
                         </div>
                         <div className="skil-icon-wrapper">
                             <i className="devicon-html5-plain leng-icon"></i>
+                            <p className="profile-skills-list-items">HTML5</p>
                         </div>
                         <div className="skil-icon-wrapper">
                             <i className="devicon-css3-plain leng-icon"></i>
+                            <p className="profile-skills-list-items">CSS3</p>
                         </div>
                         <div className="skil-icon-wrapper">
                             <i className="devicon-jquery-plain leng-icon"></i>
+                            <p className="profile-skills-list-items">jquery</p>
                         </div>
                         <div className="skil-icon-wrapper">
                             <i className="devicon-postgresql-plain leng-icon"></i>
+                            <p className="profile-skills-list-items">PostgreSQL</p>
                         </div>
                         <div className="skil-icon-wrapper">
                             <i className="devicon-bootstrap-plain leng-icon"></i>
+                            <p className="profile-skills-list-items">Bootstrap</p>
                         </div>
                         <div className="skil-icon-wrapper">
                             <i className="devicon-express-original leng-icon"></i>
+                            <p className="profile-skills-list-items">ExpressJS</p>
                         </div>
                         <div className="skil-icon-wrapper">
                             <i className="devicon-visualstudio-plain leng-icon"></i>
+                            <p className="profile-skills-list-items">VS Code</p>
                         </div>
                         <div className="skil-icon-wrapper">
                             <i className="devicon-git-plain leng-icon"></i>
+                            <p className="profile-skills-list-items">Git</p>
                         </div>
                         <div className="skil-icon-wrapper">
                             <i className="devicon-github-plain leng-icon"></i>
+                            <p className="profile-skills-list-items">GitHub</p>
                         </div>
-                        <div className="skil-icon-wrapper">
-                            <i className="devicon-gitlab-plain leng-icon"></i>
-                        </div>
-                        <div className="skil-icon-wrapper">
-                            <i className="devicon-heroku-plain-wordmark leng-icon"></i>
-                        </div> 
                         <div className="skil-icon-wrapper">
                             <i className="devicon-heroku-plain leng-icon"></i>
+                            <p className="profile-skills-list-items">Heroku</p>
+
                         </div>
                         <div className="skil-icon-wrapper">
                             <i className="devicon-amazonwebservices-original leng-icon"></i>
-                        </div>
-                        <div className="skil-icon-wrapper">
-                            <i className="devicon-angularjs-plain leng-icon"></i>
-                        </div>
-                        <div className="skil-icon-wrapper">
-                            <i className="devicon-wordpress-plain leng-icon"></i>
+                            <p className="profile-skills-list-items">AWS</p>
                         </div>
                         <div className="skil-icon-wrapper">
                             <i className="devicon-chrome-plain leng-icon"></i>
+                            <p className="profile-skills-list-items">Chrome Dev tools</p>
                         </div>  
                     </div> 
                 </BounceInRight>
